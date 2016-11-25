c62904339c4e2213bc3f77284722029805718917
q1082121 <rubbish.boy@163.com>
2016-11-25 16:30:29 +0800

    名片盒子

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-//注册小程序
+/******************************* 
+ * 名称:入口
+ * 作者:rubbishrubbish.boy@163.com
+ *******************************
+*/
 
 var config={
   //生命周期函数--监听小程序初始化
@@ -9,7 +13,6 @@ var config={
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    
     
   },
   //生命周期函数--监听小程序显示
@@ -39,8 +42,23 @@ var config={
       })
     }
   },
+  bindNavigateTo:function(action)
+  {
+    var linkurl
+    switch(action)
+    {
+      case 'business_card':
+      linkurl=this.globalData.rootdir+'/businessCard/businessCard_add'
+      break
+    }
+    wx.navigateTo({
+      url: linkurl
+    })
+  },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    domainName:"http://127.0.0.1",
+    rootdir:"/pages"
     
   }
 }
